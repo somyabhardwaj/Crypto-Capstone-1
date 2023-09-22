9a46044a3198bb705266c36ef5fe89a3900c9355
Somya Bhardwaj <somyabhardwaj004@gmail.com>
2023-09-23 00:53:56 +0530

    Time Buttons

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,6 +3,7 @@ import LineChart from '../Charts/LineChart'
 import CurrencyDropdown from '../DropDown/CurrencyDropdown'
 import Searchbar from '../DropDown/Searchbar/Searchbar'
 import PieChart from '../Charts/PieChart'
+import TimeButtons from '../Timebuttons/TimeButtons'
 
 function Home() {
   return (
@@ -13,6 +14,7 @@ function Home() {
     <Searchbar />
     </div>   
     <div className=" w-8/12  ml-12 p-2 bg-gray-100">
+    <TimeButtons />
     <LineChart />
     <PieChart />
     </div>
--- src/components/Timebuttons/TimeButtons.js
+++ src/components/Timebuttons/TimeButtons.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 function TimeButtons() {
   return (
-    <div>
-      
-    </div>
+    <>
+    <div className="flex items-center ">
+        <button className="flex items-center w-10 border border-gray-300 rounded bg-white text-gray-600 h-8 m-2 p-2 " value="1">1D</button>
+        <button className="flex items-center w-10 border border-gray-300 rounded bg-white text-gray-600 h-8 m-2 p-2 " value="7">1W</button>
+        <button className="flex items-center w-10 border border-gray-300 rounded bg-white text-gray-600 h-8 m-2 p-2 " value="14">2W</button>
+        <button className="flex items-center w-10 border border-gray-300 rounded bg-white text-gray-600 h-8 m-2 p-2 " value="30">1M</button>
+        <button className="flex items-center w-10 border border-gray-300 rounded bg-white text-gray-600 h-8 m-2 p-2 " value="180">6M</button>
+        <button className="flex items-center w-10 border border-gray-300 rounded bg-white text-gray-600 h-8 m-2 p-2 " value="360">1Y</button>
+        </div>
+    </>
   )
 }
 
